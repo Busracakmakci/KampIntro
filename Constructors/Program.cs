3c02fe630acaa8605126b0c2ef319bfd9b48e668
Büşra Çakmakcı <busracakmakci95@gmail.com>
2023-08-21 17:20:54 +0300

    21.08.23 Generic Class ilave

---
+++ Constructors/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Constructors
+{
+    class Program
+    {
+        public void Main(string[] args) {
+            Customer customer = new Customer { Id = 1, FirstName = "Engin", lastName="Çakmak", City="Ankara" };
+            Customer customer3 = new Customer();
+            customer3.Id = 2;
+            Customer customer2 = new Customer(2, "Derin", "Demir", "Ankara");
+            Console.WriteLine(customer2.FirstName);
+        }
+  
+    }
+    class Customer
+    {
+        public Customer() { }
+
+        //default constructor
+        public Customer(int id, string firstName, string lastName, string city) 
+        {
+            //yazmasak bile yapıcı blok çalışır.
+            Id = id;
+            FirstName = firstName;
+            lastName = lastName;
+            City = city;
+
+
+        } 
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Generics
+
+{ class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+       
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("izmir");
+        }
+    }
+    class MyList<T>    //Generic classs
+    {
+        public void Add(T item)
+        {
+
+        }
+
+    }
+
+}
